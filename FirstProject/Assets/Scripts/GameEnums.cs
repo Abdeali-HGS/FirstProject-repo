b4c72932aca89e117b321a714a3cfc5c9a78cde6
Abdeali-HGS <abdeali.savai@hgs.hiddengeniusproject.org>
2022-07-29 15:25:13 -0700

    Rollie Pollie
    Final Project of Abdeali Savai (Rollie Pollie)

---
+++ FirstProject/Assets/Scripts/GameEnums.cs
@@ -0,0 +1,23 @@
+//What is an enum?
+public enum MovementPattern
+{
+   NormalForward,
+   BoomerangForward,
+   BoomerangSideways,
+   ZigZagForward,   
+}
+public enum UnitTypes
+{
+   Player,
+   Enemy,
+   Obstacle,
+}
+// The code above (7-11) is to tell the code what exactly can we apply damage too
+ 
+public enum AbilityTypes
+{
+   Normal,
+   Boomerang,
+   GunAbility,
+   ZigzagAbility,
+}
